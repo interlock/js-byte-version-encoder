489faf6b9b4f69079fe808488f15db772be0ba63
James Sapara <james.sapara@gmail.com>
2017-11-06 14:26:10 -0800

    added tests

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 
 const maxWord = 0xffff;
 
+function padStart(string, count, fill) {
+  count = count || 4;
+  fill = fill || '0';
+  while(string.length < count) {
+    string = `${fill}${string}`;
+  }
+  return string;
+}
+
 /**
  * Decode a version made up of words, default is two (firmware style)
  * but supports 3 (semver style).
  */
-decodeVersion(v, words ) {
+function decodeVersion(v, words ) {
   words = words || 3;
 
   const parts = [];
@@ -25,12 +34,12 @@ decodeVersion(v, words ) {
  * Note: The dirty hack of converting each word to hex then back
  * is a work around for JS 32bit bitwise operations.
  */
-encodeVersion(v) {
+function encodeVersion(v) {
   const split = v.split('.').map(i => parseInt(i, 10));
   let out = '';
   for (let i = 0; i < split.length; i += 1) {
     const iv = split[i];
-    out += (iv).toString(16).padStart(4, '0');
+    out += padStart((iv).toString(16), 4, '0');
   }
   return parseInt(`0x${out}`, 16);
 };
@@ -38,4 +47,5 @@ encodeVersion(v) {
 module.exports = {
   decodeVersion: decodeVersion,
   encodeVersion: encodeVersion,
+  _padStart: padStart,
 };
