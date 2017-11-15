c7e41d8a62de8fbab6743054412097e05925b6db
James Sapara <james.sapara@gmail.com>
2017-11-15 14:39:05 -0800

    added eslint and fixed linting errors. No longer using ES6 features, back to ES5

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* Licensed under MIT, see LICENSE.md */
 
-const maxWord = 0xffff;
+var maxWord = 0xffff;
 
 function padStart(string, count, fill) {
   count = count || 4;
   fill = fill || '0';
   while(string.length < count) {
-    string = `${fill}${string}`;
+    string = fill + string;
   }
   return string;
 }
@@ -18,13 +18,13 @@ function padStart(string, count, fill) {
 function decodeVersion(v, words ) {
   words = words || 3;
 
-  const parts = [];
-  for (let i = 0; i < words; i += 1) {
+  var parts = [];
+  for (var i = 0; i < words; i += 1) {
     parts.push((v & (maxWord << 16 * i)) >>> 16 * i);
   }
 
   return parts.reverse().join('.');
-};
+}
 
 /**
  * Encodes a string version to a number
@@ -35,14 +35,14 @@ function decodeVersion(v, words ) {
  * is a work around for JS 32bit bitwise operations.
  */
 function encodeVersion(v) {
-  const split = v.split('.').map(i => parseInt(i, 10));
-  let out = '';
-  for (let i = 0; i < split.length; i += 1) {
-    const iv = split[i];
+  var split = v.split('.').map(function(i) { return parseInt(i, 10); });
+  var out = '';
+  for (var i = 0; i < split.length; i += 1) {
+    var iv = split[i];
     out += padStart((iv).toString(16), 4, '0');
   }
-  return parseInt(`0x${out}`, 16);
-};
+  return parseInt('0x' + out, 16);
+}
 
 module.exports = {
   decodeVersion: decodeVersion,
--- index.test.js
+++ index.test.js
@@ -1,63 +1,63 @@
-const version = require('./index');
+var version = require('./index');
 
-describe('decodeVersion', () => {
-  it('works with unpadded number', () => {
+describe('decodeVersion', function() {
+  it('works with unpadded number', function() {
     expect(version.decodeVersion(1, 2)).toEqual('0.1');
   });
 
-  it('works with zero', () => {
+  it('works with zero', function() {
     expect(version.decodeVersion(0, 2)).toEqual('0.0');
   });
 
-  it('works with max major', () => {
+  it('works with max major', function() {
     expect(version.decodeVersion(0xFFFF0000, 2)).toEqual('65535.0');
   });
 
-  it('works with max minor', () => {
+  it('works with max minor', function() {
     expect(version.decodeVersion(0x0000FFFF, 2)).toEqual('0.65535');
   });
 
-  it('works with m.n.p', () => {
+  it('works with m.n.p', function() {
     expect(version.decodeVersion(0xFFFF0000FFFF)).toEqual('65535.0.65535');
   });
 });
 
-describe('encodeVersion', () => {
-  it('works with single digit', () => {
+describe('encodeVersion', function() {
+  it('works with single digit', function() {
     expect(version.encodeVersion('12')).toEqual(12);
   });
 
-  it('works on max single', () => {
+  it('works on max single', function() {
     expect(version.encodeVersion('65535')).toEqual(65535);
   });
 
-  it('works with two digits', () => {
+  it('works with two digits', function() {
     expect(version.encodeVersion('1.14')).toEqual(0x1000E);
   });
 
-  it('works on max two digit', () => {
+  it('works on max two digit', function() {
     expect(version.encodeVersion('65535.0')).toEqual(0xffff0000);
   });
 
-  it('works with three digits', () => {
+  it('works with three digits', function() {
     expect(version.encodeVersion('1.1.14')).toEqual(0x10001000E);
   });
 
-  it('works on max three digit', () => {
+  it('works on max three digit', function() {
     expect(version.encodeVersion('65535.0.0')).toEqual(0xffff00000000);
   });
 });
 
-describe('padStart', () => {
-  it('empty string', () => {
+describe('padStart', function() {
+  it('empty string', function() {
     expect(version._padStart('')).toEqual('0000');
   });
 
-  it('number', () => {
+  it('number', function() {
     expect(version._padStart('12')).toEqual('0012');
   });
 
-  it('larger string', () => {
+  it('larger string', function() {
     expect(version._padStart('abcd')).toEqual('abcd');
   });
 });
